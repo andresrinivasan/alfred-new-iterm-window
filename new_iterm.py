66afd423e213291a5d5a83cbbb0038054f66ef10
André Srinivasan <andre.srinivasan@gmail.com>
2022-12-27 19:15:34 -0800

    RC
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- new_iterm.py
+++ new_iterm.py
@@ -6,12 +6,21 @@ import iterm2
 
 
 async def main(connection):
-    if len(sys.argv) > 1:
-        p = sys.argv[1]
-    else:
+    p = sys.argv[1]
+    if p == "ITERM_DEFAULT_PROFILE":
         p = None
 
-    w = await iterm2.Window.async_create(connection, p)
+    if len(sys.argv) > 2:
+        d = sys.argv[2]
+        l = iterm2.LocalWriteOnlyProfile()
+        l.set_initial_directory_mode(
+            iterm2.InitialWorkingDirectory.INITIAL_WORKING_DIRECTORY_CUSTOM
+        )
+        l.set_custom_directory(d)
+    else:
+        l = None
+
+    w = await iterm2.Window.async_create(connection, p, profile_customizations=l)
     await w.async_activate()  # Not sufficient to always raise the window
 
     a = await iterm2.async_get_app(connection)
